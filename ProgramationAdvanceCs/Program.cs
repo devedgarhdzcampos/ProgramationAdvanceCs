409b006cbdf5a27ee4b8b0f2c801b8efe102ca8e
Garo <edgarhdzcampos@gmail.com>
2022-01-09 02:01:08 -0600

    Se agrega una mejor estructura y se modifica el program para acceso por medio de la interfaz de cada metodo de practica

--- ProgramationAdvanceCs/Program.cs
+++ ProgramationAdvanceCs/Program.cs
@@ -7,36 +7,8 @@ namespace factoryMethodPattern
     {
         static void Main(string[] args)
         {
-            // See https://aka.ms/new-console-template for more information
-            // var creator = new ConcreteCreator();
-            // var product = creator.FactoryMethod();
-            // Console.WriteLine($"Product es del tipo: {product.GetType().Name}");
-
-            // AbstractFactory factory = new ConcreteFactory();
-            // Client client = new Client(factory);
-            // client.Run();
-
-            IFactory myFactory = new ChemicalFactory();
-            myFactory.CreateProduct();
-
-            IMilkProduct myMilk = myFactory.GetMilkProduct;
-            IFlavourProduct myFlavour = myFactory.GetFlavour;
-
-            myMilk.Production();
-            myFlavour.Get();
-
-            Console.WriteLine("Mi malteada es de {0} y {1}", 
-                myMilk.GetInformation(), myFlavour.Information());
-            Console.WriteLine("---------------------------------");
-
-            myFactory = new NaturalFactory();
-            myFactory.CreateProduct();
-
-            myMilk = myFactory.GetMilkProduct;
-            myFlavour = myFactory.GetFlavour;
-            Console.WriteLine("Mi malteada es de {0} y {1}.",
-                myMilk.GetInformation(), myFlavour.Information());
-
+            Abstract.IPracticeMain obj = new dotnet.Collections.Array.PracticeBitArray();
+            obj.Run();
         }
     }
 }
